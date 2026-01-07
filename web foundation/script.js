3e82a0c734f15eb20b8775a16402824876698c56
kishan <kishan.prajapati@innvonix.in>
2026-01-07 14:14:02 +0530

    Basic structure of implemented

---
+++ web foundation/script.js
@@ -0,0 +1,25 @@
+
+const user = {
+    name: "User",
+    skills: ["HTML", "CSS", "JS"],
+    about: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam optio iste fugiat sint necessitatibus sit quae quam fuga adipisci neque facere nemo commodi, at eum. Quasi iste quam laudantium laborum?"
+}
+
+window.addEventListener("load", () => {
+    const name = document.getElementById("name")
+    const about = document.getElementById("about")
+    const skillContainer = document.getElementById("skillContainer")
+
+    name.innerText = user.name;
+    about.innerText = user.about;
+
+    const ul = document.createElement("ul")
+
+    for(let i = 0 ; i < user.skills.length ; i++){
+        const li = document.createElement("li")
+        li.innerText = user.skills[i]
+        ul.appendChild(li)
+    }
+
+    skillContainer.appendChild(ul)
+})
