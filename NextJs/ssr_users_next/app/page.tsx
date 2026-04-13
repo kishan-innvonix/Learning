261eb1597cd77c56beda0cb9a57b5a5386db5124
kishan.prajapti <kishan.prajapati@innvonix.in>
2026-04-13 16:19:08 +0530

    Updated dashboard

--- NextJs/ssr_users_next/app/page.tsx
+++ NextJs/ssr_users_next/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Link href="/users" className="text-blue-500">
-        Users Page
+        Find Users
       </Link>
     </div>
   );
